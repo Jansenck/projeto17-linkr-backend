018a3e3b3b65b9103f0594253d84c2ba09c58c92
Jansenck <caik11@hotmail.com>
2022-10-23 13:31:43 -0300

    listing posts from database

--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -3,13 +3,19 @@ import { validationPublication } from "../schemas/user.schemas.js";
 
 async function validatePublication(req,res, next){
 
+    const token = req.headers.authorization?.replace("Bearer ", "");
+    if(!token) return res.sendStatus(StatusCodes.UNAUTHORIZED);
+
     try {
 
         const publicationContent = req.body;
         if(!publicationContent) return res.sendStatus(StatusCodes.BAD_REQUEST);
 
         const { link, description } = req.body;
-        const isValidPublication = validationPublication.validateAsync({link, description}, {abortEarly: false});
+        const isValidPublication = validationPublication.validateAsync(
+            {link, description}, 
+            {abortEarly: false}
+        );
 
         if(isValidPublication.error){
             const publicationError = isValidPublication.error.details.map(
