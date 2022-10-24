59a670cef36363ad8e653e502db6ad74dd026625
layconjohn <layconjohn@gmail.com>
2022-10-24 15:56:56 -0300

    feat: router verifyIfUserLikedPublication

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,6 +1,7 @@
 import connection from "../database/database.js";
 import { StatusCodes } from  "http-status-codes";
 
+
 async function likePublication(req, res) {
     const {publicationId} = req.params;
     const token = req.headers.authorization?.replace("Bearer ", "");
@@ -25,6 +26,28 @@ async function likePublication(req, res) {
     }
 }
 
-export {likePublication};
+async function verifyIfUserLikedPublication(req, res) {
+    const {publicationId} = req.params;
+    const token = req.headers.authorization?.replace("Bearer ", "");
+    
+    try {
+        const session = (await connection.query('SELECT * FROM sessions WHERE token = $1', [token])).rows[0];
+        if (!session) {
+            return res.sendStatus(401);
+        }
+
+        const postIsliked = (await connection.query('SELECT * FROM likes WHERE "userId" = $1 AND "publicationId" = $2;', [session.userId, publicationId])).rows[0];
+        if (postIsliked) {
+            return res.status(200).send({isLiked: true});
+        } 
+
+        return res.status(200).send({isLiked: false});
+    } catch (error) {
+        console.log(error.message);
+        return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+export {likePublication, verifyIfUserLikedPublication};
 
 
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { likePublication } from "../controllers/like.controller.js";
+import { likePublication, verifyIfUserLikedPublication } from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.post("/like/:publicationId", likePublication);
+router.get("/like/:publicationId/isLiked", verifyIfUserLikedPublication);
 
 export default router;
