3e7590b9f1a9a27a5140d2abffd28de4e565c7f6
layconjohn <layconjohn@gmail.com>
2022-10-25 22:52:14 -0300

    feat: Pagination of listPublications

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,29 +2,50 @@ import { StatusCodes } from  "http-status-codes";
 import connection from "../database/database.js";
 
 async function listPublications(req, res){
+    const page = parseInt(req.query.page);
+    
+    let start = (page * 20) - 20;
+    let limitPage = start + 20;
+    /*if (page !== undefined && page !== 1) {
+        limitPage = (page) * 3;
+        start = page + limitPage;
+    } else {
+        start = page;
+        limitPage = 3;
+    }*/
+
 
     try {
 
-        const publications = await connection.query(
+        const publications = (await connection.query(
 
             `
             SELECT
-                publications.id,
-                publications."userId",
-                u1.username,
-                publications.link,
-                publications.description,
-                u1.image AS "profilePicture",
+            publications.id,
+            publications."userId",
+            u1.username,
+            publications.link,
+            publications.description,
+            u1.image AS "profilePicture",
 
-                u2.username AS "whoLiked"
-                
+            u2.username AS "whoLiked"
+
             FROM publications 
             JOIN users u1 ON u1.id = publications."userId"
             LEFT JOIN likes ON likes."publicationId" = publications.id 
-            LEFT JOIN users u2 ON u2.id = likes."userId";`
-        );
+            LEFT JOIN users u2 ON u2.id = likes."userId"
+            ORDER BY publications.id DESC 
+            LIMIT 10
+            ;`
+        )).rows;
 
-        return res.status(StatusCodes.OK).send(publications.rows);
+        if (limitPage > publications.length) {
+            return res.status(StatusCodes.OK).send(publications.slice(-10));
+        }
+
+        const paginatedPublications = publications.slice(start, limitPage);
+
+        return res.status(StatusCodes.OK).send(paginatedPublications);
 
     } catch (error) {
         console.error(error);
